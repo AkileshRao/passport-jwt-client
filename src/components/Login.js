d5ca23b9daffcb5312eda7f5297e2ea8619ff569
akileshrao <akileshrao19@gmail.com>
2021-11-08 21:43:33 +0530

    Passport JWT client part 1

---
+++ src/components/Login.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import React, { useState } from 'react'
+
+function Login() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const submit = () => {
+        console.log(username, password)
+        axios.post("http://localhost:5000/login", { username, password }).then(user => {
+            console.log(user);
+            localStorage.setItem('token', user.data.token)
+        }).catch(err => {
+            console.log(err);
+        })
+    }
+
+    return (
+        <div>
+            <input type="text" placeholder="Enter Username" value={username} onChange={event => setUsername(event.target.value)} />
+            <input type="password" placeholder="Enter Password" value={password} onChange={event => setPassword(event.target.value)} />
+            <button onClick={submit}>Submit</button>
+        </div>
+    )
+}
+
+export default Login
---
+++ src/components/Protected.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Protected() {
+    return (
+        <div>
+            <h1>Protected</h1>
+        </div>
+    )
+}
+
+export default Protected
