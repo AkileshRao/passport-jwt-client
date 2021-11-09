a3695d77d4bd4266f01ce99255a48a130a35c0fe
akileshrao <akileshrao19@gmail.com>
2021-11-09 06:58:30 +0530

    Final

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,15 +1,33 @@
 import axios from 'axios';
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router';
 
 function Login() {
+    let navigate = useNavigate();
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        axios.get("http://localhost:5000/protected", {
+            headers: {
+                Authorization: token,
+            }
+        }).then(res => {
+            console.log(res)
+            navigate('/protected')
+        }).catch(err => {
+            console.log(err);
+            navigate('/login')
+        })
+    }, [])
 
     const submit = () => {
         console.log(username, password)
         axios.post("http://localhost:5000/login", { username, password }).then(user => {
             console.log(user);
             localStorage.setItem('token', user.data.token)
+            navigate('/protected')
         }).catch(err => {
             console.log(err);
         })
--- src/components/Protected.js
+++ src/components/Protected.js
@@ -1,6 +1,22 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router';
 
 function Protected() {
+    let navigate = useNavigate()
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        axios.get("http://localhost:5000/protected", {
+            headers: {
+                Authorization: token,
+            }
+        }).then(res => {
+            console.log(res)
+        }).catch(err => {
+            console.log(err);
+            navigate('/login')
+        })
+    }, [])
     return (
         <div>
             <h1>Protected</h1>
